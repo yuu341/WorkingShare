856a06baa126b74c37c3def0ce2163bbf38d0c07
Yusuke Furuya <oz.g.341@gmail.com>
2015-07-08 10:10:35 +0900

    normalize.css追加 menu追加 FLOCSSの適用

--- WorkingShare/App_Start/BundleConfig.cs
+++ WorkingShare/App_Start/BundleConfig.cs
@@ -11,11 +11,11 @@ namespace WorkingShare
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                "~/Scripts/jquery-{version}.js"));
+                "~/Scripts/jquery/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                "~/Scripts/jquery.unobtrusive*",
-                "~/Scripts/jquery.validate*"));
+                "~/Scripts/jquery/jquery.unobtrusive*",
+                "~/Scripts/jquery/jquery.validate*"));
 
             bundles.Add(new ScriptBundle("~/bundles/knockout").Include(
                 "~/Scripts/knockout-{version}.js",
@@ -32,18 +32,28 @@ namespace WorkingShare
             // 開発と学習には、Modernizr の開発バージョンを使用します。次に、実稼働の準備が
             // できたら、http://modernizr.com にあるビルド ツールを使用して、必要なテストのみを選択します。
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                "~/Scripts/modernizr-*"));
+                "~/Scripts/modernizr/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                "~/Scripts/bootstrap.js",
-                "~/Scripts/respond.js"));
+                "~/Scripts/bootstrap/bootstrap.js",
+                "~/Scripts/respond/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
+                 "~/Content/normalize.css",
                  "~/Content/bootstrap.css",
-                 "~/Content/Site.css"));
+                 "~/Content/Site.css",
+                 "~/Content/Foundation.css",
+                 "~/Content/Layout.css",
+                 "~/Content/Object.css"
+                 ));
+
 
             bundles.Add(new StyleBundle("~/Content/UIParts/Menu").Include(
                  "~/Content/Menu.css"
+                ));
+
+            bundles.Add(new ScriptBundle("~/bundles/UIParts/Menu").Include(
+                 "~/Scripts/menu.js"
                 ));
         }
     }
